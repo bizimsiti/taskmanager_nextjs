90cf2297d306d26aee54455ad433fadb2a70e8e9
Ali <hoccalo@gmail.com>
2024-07-17 10:58:28 +0300

    custom form components

--- app/(dashboard)/organization/[id]/form.tsx
+++ app/(dashboard)/organization/[id]/form.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import FormInput from "./input";
+
 import { useAction } from "@/hooks/useAction";
 import { createBoard } from "@/actions/createBoard/";
+import FormInput from "@/components/form/formInput";
+import FormSubmit from "@/components/form/formSubmit";
 type Props = {};
 
 const Form = (props: Props) => {
@@ -18,17 +20,17 @@ const Form = (props: Props) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    console.log("fielderrors", fieldErrors);
+
     execute({ title });
   };
 
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput label="Board Title" id="title" errors={fieldErrors} />
       </div>
-      <Button variant="default" size="sm" type="submit">
-        Submit
-      </Button>
+      <FormSubmit variant="primary">Save</FormSubmit>
     </form>
   );
 };
---
+++ components/form/formSubmit.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { useFormStatus } from "react-dom";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+};
+
+const FormSubmit = ({ children, className, disabled, variant }: Props) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      disabled={pending || disabled}
+      className={cn(className)}
+      variant={variant}
+      size="sm"
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default FormSubmit;
