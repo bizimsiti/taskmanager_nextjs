7733f33d0d921f59d1566dd80fc33f8ec504b07f
Ali <hoccalo@gmail.com>
2024-07-19 10:52:56 +0300

    metadata lodash

--- app/(dashboard)/organization/[id]/layout.tsx
+++ app/(dashboard)/organization/[id]/layout.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import OrgControl from "./components/org-control";
-
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
 type Props = {
   children: React.ReactNode;
 };
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization")
+  };
+}
 
 const layout = ({ children }: Props) => {
   return (
