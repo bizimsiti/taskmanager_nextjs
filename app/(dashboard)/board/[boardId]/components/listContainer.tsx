5dac47e86e48dd569580f8db893573044c9bba53
Ali <hoccalo@gmail.com>
2024-07-31 15:28:58 +0300

    list header update action and ui

--- app/(dashboard)/board/[boardId]/components/listContainer.tsx
+++ app/(dashboard)/board/[boardId]/components/listContainer.tsx
@@ -1,6 +1,7 @@
 import { ListWithCards } from "@/types";
 import React from "react";
 import ListForm from "./listForm";
+import ListItem from "./listItem";
 
 type Props = {
   data: ListWithCards[];
@@ -9,7 +10,10 @@ type Props = {
 
 const ListContainer = ({ data, boardId }: Props) => {
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {data.map((list, index) => (
+        <ListItem index={index} data={list} key={index} />
+      ))}
       <ListForm />
     </ol>
   );
