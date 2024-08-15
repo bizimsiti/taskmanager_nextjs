65affb2919e322b211f1321cb4d5e6fec53c7b1d
Ali <hoccalo@gmail.com>
2024-08-15 15:52:05 +0300

    card actions

--- components/modals/cardModal/index.tsx
+++ components/modals/cardModal/index.tsx
@@ -6,6 +6,7 @@ import { Card } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import Header from "./header";
+import Description from "./description";
 
 type Props = {};
 
@@ -21,6 +22,17 @@ const CardModal = (props: Props) => {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
